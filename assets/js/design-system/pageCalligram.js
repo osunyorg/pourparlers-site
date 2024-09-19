c4adf9b6094b2b4bb9646147c9fb44daa9f21d9d
Olivia206 <olivia.simonet206@gmail.com>
2024-09-19 17:45:46 +0200

    use js for better randomization of calligrams

---
+++ assets/js/design-system/pageCalligram.js
@@ -0,0 +1,29 @@
+class PageCalligram {
+    constructor () {
+        this.calligram = document.querySelector('.calligram');
+        if (!this.calligram) {
+            return;
+        }
+        this.images = [
+            { url: "/assets/images/calligrammes/calligramme-1.svg", orientation: "landscape" },
+            { url: "/assets/images/calligrammes/calligramme-2.svg", orientation: "portrait" },
+            { url: "/assets/images/calligrammes/calligramme-3.svg", orientation: "landscape" },
+            { url: "/assets/images/calligrammes/calligramme-4.svg", orientation: "landscape" }
+        ];
+        
+        this.listen();
+    }
+
+    listen () {
+        this.randomImage = Math.floor(Math.random() * this.images.length);
+        this.image = this.images[this.randomImage];
+
+        this.setCalligram();
+    }
+    setCalligram () {
+        this.calligram.src = this.image.url;
+        this.calligram.className += this.image.orientation;
+    }
+}
+
+export default new PageCalligram();
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,4 @@ import './vendors/lightbox';
 import './theme/';
 
 import './design-system/homeHero';
+import './design-system/pageCalligram';
